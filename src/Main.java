341cfc3ebac4a957efd5ff31984a1fbeb26868d5
Tad <tad@spotco.us>
2018-07-07 02:31:46 -0400

    Tweaks

--- src/Main.java
+++ src/Main.java
@@ -56,6 +56,7 @@ public class Main {
                         arrBlocklists.add(line.replaceAll(";", ""));
                     }
                 }
+                scanner.close();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
